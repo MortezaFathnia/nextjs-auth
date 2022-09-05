06c8dc83546ab01a277470aea2dc3c6bf1920c45
MortezaFathnia <fathniamorteza@gmail.com>
2022-09-05 20:05:32 +0430

    changing password add

--- components/layout/main-navigation.js
+++ components/layout/main-navigation.js
@@ -1,8 +1,15 @@
+import { signOut, useSession } from 'next-auth/react';
 import Link from 'next/link';
 
 import classes from './main-navigation.module.css';
 
 function MainNavigation() {
+  const { data: session, status } = useSession()
+  console.log(session)
+  console.log(status)
+  function logoutHandler() {
+    signOut({ callbackUrl: '/auth' });
+  }
   return (
     <header className={classes.header}>
       <Link href='/'>
@@ -12,15 +19,23 @@ function MainNavigation() {
       </Link>
       <nav>
         <ul>
-          <li>
-            <Link href='/auth'>Login</Link>
-          </li>
-          <li>
-            <Link href='/profile'>Profile</Link>
-          </li>
-          <li>
-            <button>Logout</button>
-          </li>
+          {!session && status !== 'loading' && (
+            <li>
+              <Link href='/auth'>Login</Link>
+            </li>
+          )}
+
+          {status === "authenticated" && (
+            <>
+              <li>
+                <Link href='/profile'>Profile</Link>
+              </li>
+              <li>
+                <button onClick={logoutHandler}>Logout</button>
+              </li>
+            </>
+          )}
+
         </ul>
       </nav>
     </header>
--- components/profile/user-profile.js
+++ components/profile/user-profile.js
@@ -4,10 +4,24 @@ import classes from './user-profile.module.css';
 function UserProfile() {
   // Redirect away if NOT auth
 
+  async function changePasswordHandler(passwordData) {
+    const response = await fetch('/api/user/change-password', {
+      method: "PATCH",
+      body: JSON.stringify(passwordData),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+
+    const data = await response.json();
+
+    console.log(data);
+  }
+
   return (
     <section className={classes.profile}>
       <h1>Your User Profile</h1>
-      <ProfileForm />
+      <ProfileForm onChangePassword={changePasswordHandler} />
     </section>
   );
 }
